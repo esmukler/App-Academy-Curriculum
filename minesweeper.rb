3e6cdb027aad0b03048f75d2e2205b06cedd29d7
Eli and Jake <jakeandeli@appacademy.io>
2015-01-26 13:12:39 -0500

    Create most of board generation (bug hitting bracket methods)

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,8 +1,55 @@
+require 'byebug'
+class Board
+  attr_reader :tiles
+
+  def initialize
+    @tiles = Array.new(9) { Array.new(9) { nil } }
+  end
+
+  def set_random_bombs
+    bomb_tiles = []
+    until bomb_tiles.count == 10
+      bomb_tile = [rand(9), rand(9)]
+      bomb_tiles << bomb_tile unless bomb_tiles.include?(bomb_tile)
+    end
+    bomb_tiles.each do |coordinates|
+      self[coordinates] = Tile.new(true, coordinates)
+    end
+  end
+
+  def populate_board
+    set_random_bombs
+    fill_board
+  end
+
+  def fill_board
+    @tiles.each_with_index do |rows, y|
+      rows.each_with_index do |tile, x|
+        self[[x,y]] = Tile.new(false, [x,y]) if tile.nil?
+      end
+    end
+    @tiles.each do |row|
+      row.each do |tile|
+        tile.get_neighbors(self)
+      end
+    end
+  end
+
+  def []=(coordinates, tile)
+    @tiles[coordinates[1]][coordinates[0]] = tile
+  end
+
+  def [](x,y)
+    debugger
+    @tiles[x][y]
+  end
+end
+
 class Tile
-  attr_accessor :revealed, :flagged, :bomb
+  attr_accessor :revealed, :flagged, :bomb, :coordinates
 
   def initialize(bomb, coordinates)
-    @revealed = false
+    @revealed = true
     @flagged = false
     @bomb = bomb
     @neighbors = []
@@ -13,8 +60,8 @@ class Tile
     @neighbors.select{|neighbor| neighbor.bomb}.count
   end
 
-  def display
-    unless @revealed
+  def inspect
+    if !@revealed
       return "*"
     elsif @flagged
       return "F"
@@ -26,6 +73,8 @@ class Tile
   end
 
   def get_neighbors(board)
+    x = @coordinates[0]
+    y = @coordinates[1]
     @neighbors = [
     board[x+1, y+1],
     board[x-1, y+1],
@@ -35,7 +84,7 @@ class Tile
     board[x, y-1],
     board[x+1, y],
     board[x-1, y],
-  ]
+    ]
+    @neighbors.select! { |neighbor| neighbor }
   end
-
 end
