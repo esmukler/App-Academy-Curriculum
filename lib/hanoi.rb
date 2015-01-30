607ba29f921f28a28c3bd4be49155ded5a929226
Eli Smukler <eli@smukler.com>
2015-01-30 14:41:53 -0500

    finish hanoi.spec

--- lib/hanoi.rb
+++ lib/hanoi.rb
@@ -6,9 +6,28 @@ class Hanoi
   end
 
   def move(start, finish)
-    raise IllegalMoveError.new if @towers[start].empty?
+    start_value, finish_value = @towers[start].last, @towers[finish].last
+
+    if start_value.nil?
+      raise IllegalMoveError.new
+    elsif !finish_value.nil? && start_value > finish_value
+      raise IllegalMoveError.new
+    end
+
     disc = @towers[start].pop
     @towers[finish].push(disc)
+  end
+
+  def win?
+    towers[0].empty? && (towers[1].empty? || towers[2].empty?)
+  end
+
+  def get_move
+    input = gets
+    input.chomp! unless input.nil?
+    output = input.split(",").map(&:to_i)
+    raise ArgumentError.new if output.any? { |el| !el.between?(0,2) }
+    output
   end
 
 
--- spec/hanoi_spec.rb
+++ spec/hanoi_spec.rb
@@ -15,24 +15,63 @@ describe Hanoi do
 
   end
   describe "#move" do
+    let(:game) { Hanoi.new([[3],[],[2,1]]) }
+
     it "moves disc to an empty space" do
-      game = Hanoi.new([[3],[],[2,1]])
       game.move(0,1)
       expect(game.towers).to eq([[], [3], [2,1]])
     end
 
     it "moves a smaller disc on top of a larger disc" do
-      game = Hanoi.new([[3],[],[2,1]])
       game.move(2,0)
       expect(game.towers).to eq([[3,1], [], [2]])
     end
 
     it "raises an error if you try to move from an empty tower" do
-      game = Hanoi.new([[3],[],[2,1]])
       expect { game.move(1,2) }.to raise_error(IllegalMoveError)
     end
 
-
-
+    it "raises an error if you try to move a larger disc onto a smaller disc" do
+      expect { game.move(0,2) }.to raise_error(IllegalMoveError)
+    end
   end
+
+  describe "#win?" do
+
+    it "returns true when all 3 discs are in a new stack" do
+      game = Hanoi.new([[],[3,2,1],[]])
+      expect(game.win?).to be(true)
+    end
+
+    it "returns false when all 3 discs are on the start stack" do
+      game = Hanoi.new
+      expect(game.win?).to be(false)
+    end
+
+    it "returns false if more than one tower has discs" do
+      game = Hanoi.new([[],[3],[2,1]])
+      expect(game.win?).to be(false)
+    end
+  end
+
+  describe "#get_move" do
+    let(:game) { Hanoi.new }
+
+    it "calls the gets method" do
+      expect(game).to receive(:gets).and_return("1,2")
+      game.get_move
+    end
+
+    it "returns an array of two numbers" do
+      allow(game).to receive(:gets).and_return("1,2")
+      expect(game.get_move).to eq([1,2])
+    end
+
+    it "raises an error if numbers are out of the range (0..2)" do
+      allow(game).to receive(:gets).and_return("5,0")
+      expect { game.get_move }.to raise_error(ArgumentError)
+    end
+  end
+
+
 end
