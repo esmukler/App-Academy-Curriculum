033429d0836965c292ed27e127901de6a92bce39
Eli Smukler <eli@smukler.com>
2015-01-30 15:03:16 -0500

    finish array rspec exercises

--- lib/array.rb
+++ lib/array.rb
@@ -1,6 +1,38 @@
 
 class Array
 
+  def stock_picker
+    max_value = 0
+    best_pair = []
+
+    self.each_index do |i|
+      self.each_index do |j|
+        next unless i < j
+        diff = self[j] - self[i]
+        if diff > max_value
+          best_pair = [i,j]
+          max_value = diff
+        end
+      end
+    end
+
+    best_pair
+  end
+
+
+  def my_transpose
+    return [] if self.empty?
+    raise ArgumentError.new unless self.all? { |el| el.is_a?(Array) }
+    output = Array.new(self[0].length) { Array.new(self.length) }
+    self.each_index do |x|
+      self[x].each_with_index do |el, y|
+        output[y][x] = el
+      end
+    end
+
+    output
+  end
+
   def two_sum
     output = []
     self.each_with_index do |el, i|
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,6 +3,59 @@ require 'rspec'
 
 describe Array do
 
+
+  describe "#stock_picker" do
+
+    it "returns the pair of indices whose elements have the greatest positive differnce" do
+      expect([1,2,6,100].stock_picker).to eq([0,3])
+    end
+
+    it "uses a buy index that is before the sell index" do
+      expect([100,2,6,1].stock_picker).to eq([1,2])
+    end
+
+    it "returns an empty array if there is no profitable pair" do
+      expect([100,5,2,1].stock_picker).to eq([])
+    end
+
+  end
+
+  describe "#my_transpose" do
+
+    it "returns an array with transposed columns and rows" do
+        rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+        ]
+        expect(rows.my_transpose).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+    end
+
+    it "works on an empty array" do
+      expect([].my_transpose).to eq([])
+    end
+
+    it "works for all rectangular matrices" do
+      rect = [
+      [0, 1, 2],
+      [3, 4, 5]
+      ]
+      expect(rect.my_transpose).to eq([[0, 3],[1, 4],[2, 5]])
+    end
+
+    it "works for a 1x1 matrix" do
+      expect([[1]].my_transpose).to eq([[1]])
+    end
+
+    it "raises an error if called on a non-nested array" do
+      expect { [1,2].my_transpose }.to raise_error(ArgumentError)
+    end
+
+  end
+
+
+
+
   describe "#two_sum" do
     it "returns index pairs that sum to zero" do
       expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
