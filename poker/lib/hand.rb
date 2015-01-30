c7a26dccc297d69d1c8373769f29b3dfb5ddfae4
Eli Smukler <eli@smukler.com>
2015-01-30 17:49:08 -0500

    develop logic for hand class, e.g. flush? method

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -2,10 +2,11 @@ require_relative 'deck'
 
 
 class Hand
-  attr_reader :cards
-  def initialize(deck)
+  attr_accessor :cards
+
+  def initialize(deck, cards = nil)
     @deck = deck
-    @cards = []
+    @cards = cards || []
   end
 
   def draw
@@ -14,5 +15,54 @@ class Hand
     end
   end
 
+  def discard(indices)
+    discarded = []
+
+    indices.each do |i|
+      discarded << @cards[i]
+      cards[i] = nil
+    end
+
+    @cards.compact!
+
+    @deck.receive_cards(discarded)
+  end
+
+  def straight_flush?
+    flush? && straight?
+  end
+
+  def full_house?
+
+  end
+
+  def flush?
+    @cards.map(&:suit).uniq.count == 1
+  end
+
+  def straight?
+    card_values = @cards.map(&:poker_value).sort
+    if card_values.include?(14)
+      #aces
+    else
+
+    end
+
+
+  end
+
+  def high_card_value
+
+    #if you'er a striaght etc.
+      #we do this
+
+    #else
+      #we do this
+
+  end
+
+  def num_of_a_kind
+
+  end
 
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -2,10 +2,10 @@ require 'rspec'
 require 'hand'
 
 describe Hand do
-  describe "#draw" do
-    let(:deck) { double("deck") }
-    let(:hand) { Hand.new(deck) }
+  let(:deck) { double("deck") }
 
+  describe "#draw" do
+    let(:hand) { Hand.new(deck) }
     it "calls the Deck#deal method" do
       expect(deck).to receive(:deal).at_least(1).times.and_return([Card.rand])
       hand.draw
@@ -23,7 +23,60 @@ describe Hand do
       hand.draw
       expect(hand.cards.count).to be(5)
     end
+  end
 
+  describe "#discard" do
+    let(:hand) { Hand.new(deck) }
+
+    before(:each) do
+      hand.cards = [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                    Card.new(:spades, :four), Card.new(:spades, :five),
+                    Card.new(:spades, :six)]
+    end
+
+    it "removes the cards from the hand" do
+      allow(deck).to receive(:receive_cards)
+      hand.discard([0,1,2,3])
+      expect(hand.cards.first==(Card.new(:spades, :six))).to be(true)
+    end
+
+    it "sends the discarded cards to the deck" do
+      expect(deck).to receive(:receive_cards)
+      hand.discard([0,1,2,3])
+    end
+
+  end
+
+
+  describe "#flush?" do
+    it "returns true when a hand is a flush" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                    Card.new(:spades, :four), Card.new(:spades, :eight),
+                    Card.new(:spades, :six)])
+      expect(hand.flush?).to be(true)
+    end
+
+    it "returns false when a hand is not a flush" do
+      hand = Hand.new(deck, [Card.new(:spades, :deuce), Card.new(:spades, :three),
+                    Card.new(:hearts, :four), Card.new(:spades, :five),
+                    Card.new(:spades, :six)])
+      expect(hand.flush?).to be(false)
+    end
+  end
+  describe "#straight?" do
+    it "returns true when a hand is a straight" do
+      hand = Hand.new(deck, [Card.new(:spades, :ten), Card.new(:spades, :jack),
+                    Card.new(:heart, :ace), Card.new(:spades, :queen),
+                    Card.new(:spades, :king)])
+      expect(hand.straight?).to be(true)
+    end
+
+    it "returns false when a hand is not a straight" do
+      hand = Hand.new(deck, [Card.new(:spades, :ace), Card.new(:spades, :three),
+                    Card.new(:hearts, :four), Card.new(:spades, :king),
+                    Card.new(:spades, :six)])
+      expect(hand.straight?).to be(false)
+    end
   end
 
 end
