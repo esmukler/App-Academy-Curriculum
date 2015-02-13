73deb7ef9cb9c8b15b71d47cca51910659c52284
Eli Smukler <eli@smukler.com>
2015-02-13 10:41:47 -0500

    create user model

--- Gemfile
+++ Gemfile
@@ -32,8 +32,14 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+
+end
+
 # Use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.1.2'
+gem 'bcrypt', '~> 3.1.2'
 
 # Use unicorn as the app server
 # gem 'unicorn'
---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ActiveRecord::Base
+  after_initialize :ensure_session_token
+
+  validates :username, :session_token, :password_digest, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6}, allow_nil: true
+
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+
+    if user.is_password?(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password
+    @password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.base64
+    self.save!
+    self.session_token
+  end
+
+  private
+
+    def ensure_session_token
+      self.session_token ||= SecureRandom.base64
+    end
+end
