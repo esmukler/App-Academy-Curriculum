219aaa5b302ad21e221b00e7de1bcc31362a60e7
= <=>
2015-02-25 15:12:44 -0500

    Finish Towers of Hanoi gameplay

--- towerOfHanoi/skeleton/js/towers-view.js
+++ towerOfHanoi/skeleton/js/towers-view.js
@@ -1,59 +1,73 @@
 (function () {
-  if (typeof TTT === "undefined") {
-    window.TTT = {};
+  if (typeof Towers === "undefined") {
+    window.Towers = {};
   }
 
-  var View = TTT.View = function (game, $el) {
+  var View = Towers.View = function (game, $el) {
     this.game = game;
     this.$el = $el;
     this.bindEvents();
-    this.setupBoard();
+    this.setupTowers();
+    this.selected = 0;
+    this.startTower = null;
+  };
+
+  View.prototype.setupTowers = function ( ) {
+    var html = "<ul class='grid group'>" +
+                "<li class='small'></li><li class='empty'></li><li class='empty'></li>" +
+                "<li class='medium'></li><li class='empty'></li><li class='empty'></li>" +
+                "<li class='large'></li><li class='empty'></li><li class='empty'></li></ul>"
+    this.$el.html(html);
   };
 
   View.prototype.bindEvents = function () {
     var view = this;
     this.$el.on("click", "li", function(event) {
+      console.log(view.selected)
 
       var $square = $(event.currentTarget);
-      if ($square.text() === "") {
-        view.makeMove($square);
-      }
-    })
-  };
+      var index = $square.index() % 3;
 
-  View.prototype.makeMove = function ($square) {
-    var index = $square.index();
-    var pos = [Math.floor(index/3), index%3 ]
-    var game = this.game;
+      if (view.selected === 0) {
+        view.startTower = index;
+        view.selected = 1
+        view.getStartPiece().toggleClass("selected");
+      } else if (view.selected === 1 &&
+                 view.game.isValidMove(view.startTower, index)){
 
-    if (game.currentPlayer === 'x'){
-      $square.addClass("selectedX");
-      $square.text("X");
-    } else {
-      $square.addClass("selectedO");
-      $square.text("O");
-    }
+        view.game.move(view.startTower, index);
+        view.selected = 0;
 
-    game.playMove(pos);
+        var $startPiece = view.getStartPiece();
+        var $endPiece = view.getEndPiece(index);
 
-    if (game.isOver()) {
-      this.$el.append($("<p>"));
-
-      if (game.winner()) {
-        this.$el.find("p").text(game.winner() + " has won!");
+        $startPiece.toggleClass("selected");
+        var size = $startPiece.attr("class");
+        $startPiece.toggleClass("empty " + size);
+        $endPiece.toggleClass("empty " + size);
 
       } else {
-        this.$el.find("p").text("NO ONE WINS!");
+        view.getStartPiece().toggleClass("selected");
+        view.selected = 0;
       }
-      this.$el.off();
+
+    });
+  };
+
+  View.prototype.getStartPiece = function() {
+    for( var i = this.startTower; i < 9; i += 3){
+      if( !this.$el.find("li").eq(i).hasClass("empty")){
+        return $("li").eq(i);
+      }
     }
   };
 
-  View.prototype.setupBoard = function ( ) {
-    var html = "<ul class='grid group'><li></li><li></li><li></li>" +
-                    "<li></li><li></li><li></li>" +
-                    "<li></li><li></li><li></li></ul>"
-    this.$el.html(html);
-
+  View.prototype.getEndPiece = function(index) {
+    for( var i = index + 6; i >= 0; i -= 3){
+      if( this.$el.find("li").eq(i).hasClass("empty")){
+        return this.$el.find("li").eq(i);
+      }
+    }
   };
+
 })();
