ea46f160ad8dec9fb2d550e7efc7a779125edd9e
Eli Smukler <eli@smukler.com>
2015-02-26 18:05:06 -0500

    Initial skeleton for move gutter.

--- thumbnails/thumbnails.js
+++ thumbnails/thumbnails.js
@@ -6,24 +6,33 @@ $.Thumbnails = function (el) {
   this.$el.find('.gutter-images').on('click', 'img', this.selectImage.bind(this))
   this.$el.find('.gutter-images').on('mouseenter', 'img', this.hoverSelect.bind(this))
   this.$el.find('.gutter-images').on('mouseleave', 'img', this.activate.bind(this))
-
+  // this.$el.find('.left').on('click', this.moveGutter.bind(this, -1))
+  // this.$el.find('.right').on('click', this.moveGutter.bind(this, 1))
 };
 
 $.Thumbnails.prototype.activate = function() {
   var source = this.$activeImg.attr('src');
   $('.active:first-child').children().attr('src', source);
-}
+};
 
 $.Thumbnails.prototype.selectImage = function(event) {
   this.$activeImg = $(event.currentTarget);
   this.activate();
-}
+};
 
 $.Thumbnails.prototype.hoverSelect = function(event) {
   this.$tempImg = $(event.currentTarget);
   var source = this.$tempImg.attr('src');
   $('.active:first-child').children().attr('src', source);
-}
+};
+
+// $.Thumbnails.prototype.moveGutter = function (dir) {
+//   if (dir === 1) {
+//     this.$el.find('.active').eq(0).removeClass('active')
+//     console.log(this.$el.find('.active').eq(0));
+//     console.log(this.$el.find('.active').eq(4));
+//   }
+// };
 
 $.fn.thumbs = function () {
   return this.each(function () {
