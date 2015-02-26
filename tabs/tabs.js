b90bb920bd309dc82106653ac1014965bcfd0684
Eli Smukler <eli@smukler.com>
2015-02-26 10:49:27 -0500

    Finish swapping panes functionality

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,8 +1,25 @@
 $.Tabs = function (el) {
-  
-
-
+  this.$el = $(el);
+  this.$contentTabs = this.$el.find(".data-content-tabs");
+  this.$activeTab = this.$el.find(".tab-pane.active");
+  this.clickTab();
 };
+
+$.Tabs.prototype.clickTab = function () {
+  var tab = this;
+  this.$contentTabs.on("click", "a", function (event) {
+    event.preventDefault();
+    
+    tab.$contentTabs.find(".active").toggleClass();
+    $(event.currentTarget).toggleClass("active");
+
+    tab.$activeTab.toggleClass("active");
+    var id = event.currentTarget.hash;
+    tab.$activeTab = $(tab.$el.find(id));
+    tab.$activeTab.toggleClass("active");
+
+  })
+}
 
 $.fn.tabs = function () {
   return this.each(function () {
