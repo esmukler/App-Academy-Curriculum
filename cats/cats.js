c3d5b3fb056bb68ac1a6f681b10461b9634cdf3c
Eli Smukler <eli@smukler.com>
2015-02-26 14:44:39 -0500

    Fixed wrapping for slides

--- cats/cats.js
+++ cats/cats.js
@@ -12,17 +12,42 @@ $.Carousel = function (el) {
 
 $.Carousel.prototype.slide = function (dir) {
   this.activeIdx += dir;
+  var rightIdx = this.activeIdx + 1;
+  var leftIdx = this.activeIdx - 1;
+
+  // wrapping
   if (this.activeIdx > this.numImgs) {
     this.activeIdx = 0;
+    rightIdx = 1;
   } else if (this.activeIdx < 0) {
+    leftIdx = this.numImgs - 1;
     this.activeIdx = this.numImgs;
+  } else if (rightIdx > this.numImgs) {
+    rightIdx = 0;
+  } else if (leftIdx < 0) {
+    leftIdx = this.numImgs;
   }
 
 
-  this.$el.find("li.active").removeClass("active");
+  if (dir === 1) {
+    this.$el.find("li.active.left").removeClass("active left")
+    var $rightImg = this.$el.find("ul > li").eq(rightIdx);
+    $rightImg.addClass("active right");
+    var $newImg = this.$el.find("ul > li").eq(this.activeIdx);
+    $newImg.removeClass("right");
+    var $leftImg = this.$el.find("ul > li").eq(leftIdx);
+    $leftImg.addClass("left");
+  } else {
+    this.$el.find("li.active.right").removeClass("active right")
+    var $leftImg = this.$el.find("ul > li").eq(leftIdx);
+    $leftImg.addClass("active left");
+    var $newImg = this.$el.find("ul > li").eq(this.activeIdx);
+    $newImg.removeClass("left");
+    var $rightImg = this.$el.find("ul > li").eq(rightIdx);
+    $rightImg.addClass("right");
+  }
 
-  var $newImg = this.$el.find("ul > li").eq(this.activeIdx);
-  $newImg.addClass("active");
+  console.log(this.$el.find("li"))
 }
 
 
