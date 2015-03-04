b60ef469d125582e979a5edd33ca818b51db51de
Eli Smukler <eli@smukler.com>
2015-03-04 18:05:09 -0500

    Finish phase 6: Fancy edit

--- JournalApp/app/assets/javascripts/views/post_show.js
+++ JournalApp/app/assets/javascripts/views/post_show.js
@@ -6,7 +6,15 @@ JournalApp.Views.PostShow = Backbone.View.extend({
   tagName: "article",
 
   events: {
-    "click button.delete": "deletePost"
+    "click button.delete": "deletePost",
+    "dblclick p.post-title" : "editPostTitle",
+    "dblclick p.post-body" : "editPostBody",
+    "dblclick input.edit-title" : "editPostTitle",
+    "dblclick textarea.edit-body" : "editPostBody",
+  },
+
+  initialize: function(){
+    this.listenTo(this.model, "change", this.render);
   },
 
   id: function () {
@@ -25,5 +33,40 @@ JournalApp.Views.PostShow = Backbone.View.extend({
     this.$el.append(this.template({ post: this.model }));
     return this;
   },
+
+  editPostTitle: function(event) {
+    event.preventDefault();
+    this.$el.find(".post-title").toggleClass("hidden");
+    this.$el.find(".edit-title").toggleClass("hidden");
+    this.updatePost(this.$el.find(".edit-title"));
+  },
+
+  editPostBody: function(event) {
+    event.preventDefault();
+    this.$el.find(".post-body").toggleClass("hidden");
+    this.$el.find(".edit-body").toggleClass("hidden");
+
+    this.updatePost(this.$el.find(".edit-body"));
+  },
+
+  updatePost: function(el) {
+    var formData = el.serializeJSON();
+    console.log(formData);
+    this.model.save(formData.post, {
+      patch: true,
+      success: function() {
+        console.log("success");
+      }
+    });
+  }
+
+  // keyAction: function(event){
+  //   console.log(event.keyCode);
+  //   if (event.keyCode === 13){
+  //     this.$el.find(".post-title").removeClass("hidden");
+  //     this.$el.find(".edit-title").removeClass("hidden");
+  //     this.$el.find(".edit-title").addClass("hidden");
+  //   }
+  // }
 
 });
