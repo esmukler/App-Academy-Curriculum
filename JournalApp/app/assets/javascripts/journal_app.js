e737f54a569c1b9e46183068b23777009defc84c
Eli Smukler <eli@smukler.com>
2015-03-04 10:20:29 -0500

    Fix basic backbone syntax errors on initialize

--- JournalApp/app/assets/javascripts/journal_app.js
+++ JournalApp/app/assets/javascripts/journal_app.js
@@ -5,30 +5,29 @@ window.JournalApp = {
   Routers: {},
   initialize: function() {
     alert('Hello from Backbone!');
-  },
-
-  JournalApp.Models.Post = Backbone.Model.extend{
-    urlRoot: "/posts",
-  },
-
-  JournalApp.Collections.Posts = Backbone.Collection.extend{
-    url: "/posts",
-    model: JournalApp.Models.Post,
-    getOrFetch : function(id){
-      if (this.get(id)) {
-        this.get(id).fetch();
-
-      } else {
-        var newModel = new JournalApp.Models.Post({id: id});
-        newModel.fetch() {
-          success: this.add(newModel)
-
-        }.bind(this);
-      }
-    }.bind(this)
   }
-
 };
+
+JournalApp.Models.Post = Backbone.Model.extend({
+  urlRoot: "/posts"
+});
+
+JournalApp.Collections.Posts = Backbone.Collection.extend({
+  url: "/posts",
+  model: JournalApp.Models.Post,
+  getOrFetch : function(id){
+    if (this.get(id)) {
+      this.get(id).fetch();
+
+    } else {
+      var newModel = new JournalApp.Models.Post({id: id});
+      newModel.fetch({
+        success: this.add(newModel)
+
+      });
+    }
+  }.bind(this)
+});
 
 $(document).ready(function(){
   JournalApp.initialize();
